e10595fef5e8422681a3718ea2d65da0150dd9ec
Codevil <devilqueenlove007@gmail.com>
2025-06-01 09:40:48 +0530

    fixing errors.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss({
-      config: './tailwind.config.js', // Explicitly point to your config
-    }),
-  ],
+  plugins: [react()],
 })
